cf88bc2e7b80ee1f2e1046c970de14784f0d413b
root <root@amistad.smtp.xxx>
2026-06-29 19:01:37 +0200

    fix gentoo-install issue 153

--- run.sh
+++ run.sh
@@ -7,5 +7,11 @@ cd /tmp
 git clone https://github.com/oddlama/gentoo-install
 cp $pwd/gentoo.conf /tmp/gentoo-install/
 cd gentoo-install
+
+#Fixup: issue: https://github.com/oddlama/gentoo-install/issues/153
+export DEBUGINFOD_URLS="http://foo.mynet.local/debuginfo https://debuginfod.elfutils.org/" 
+export DEBUGINFOD_IMA_CERT_PATH="/etc/certs" 
+##
+
 ./install.sh
 
